dcd08f453e86bb21f1d14bbf30b69cbbf6bc4772
Rudy Chung <rudy_chung@alphanetworks.com>
2015-06-05 18:14:39 +0800

    prepare for conflict

--- hello.cpp
+++ hello.cpp
@@ -7,19 +7,9 @@ int main(int /*argc*/ , char * /*args*/[])
 {
 	string message("Hello, world!!");
 	cout << message << endl;
-
-    cout << "check in procedure" << endl;
-    cout << "  rudy: hello from rudy" << endl;
-    cout << "  eric: hello from eric..." << endl;
-    cout << " cyril: hello from cyril" << endl;
-    cout << " james: hello from james" << endl;
-    cout << " ellen: This is hello world from ellen." << endl;
-    cout << "  tito: from tito" << endl;
-    cout << "  lulu: This is hello world from lulu. " << endl;
-    cout << "steven: This is hello world from steven." << endl;
-    cout << "mouris: hello from mouris." << endl;
-    cout << " chris:from chris" << endl;
-    cout << "  luke: " << endl;
+    
+    cout << "resolved confilct" << endl;
+    cout << "all: " << endl;
 
 	int exitCode = 0;
 	return exitCode;
