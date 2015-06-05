8bded36c532b9aca69763b1591702b75e9a6ac60
tito zhang <tito_zhang@alphanetworks.com>
2015-06-05 18:34:36 +0800

    tito

--- hello.cpp
+++ hello.cpp
@@ -9,7 +9,7 @@ int main(int /*argc*/ , char * /*args*/[])
 	cout << message << endl;
     
     cout << "resolved confilct" << endl;
-    cout << "all: rudy1 rudy2 cyril eric mouris james" << endl;
+    cout << "all: rudy1 rudy2 cyril eric mouris james tito" << endl;
 
 	int exitCode = 0;
 	return exitCode;
