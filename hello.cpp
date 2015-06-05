698e169d724a95782a179501d6f6ddecd7e8791a
eric hsieh <eric_hsieh@alphanetworks.com>
2015-06-05 18:20:23 +0800

    eric

--- hello.cpp
+++ hello.cpp
@@ -9,7 +9,7 @@ int main(int /*argc*/ , char * /*args*/[])
 	cout << message << endl;
     
     cout << "resolved confilct" << endl;
-    cout << "all: rudy1 rudy2 cyril" << endl;
+    cout << "all: rudy1 rudy2 cyril eric" << endl;
 
 	int exitCode = 0;
 	return exitCode;
